16be4f4f188279d2d93abe736d2716109d3118d1
Night Lore <nathanlui2010@gmail.com>
2020-05-21 22:59:39 -0700

    Playing with the FBO shader

--- resources/tex_fbo_frag.glsl
+++ resources/tex_fbo_frag.glsl
@@ -8,29 +8,72 @@ uniform float fTime;
 uniform vec3 targetPos;
 
 void main(){
-	vec3 blue = vec3(0.1, 0.1, 0.2);
-	vec3 yellow = vec3(0.2, 0.2, 0.0);
+	vec3 blue = vec3(0.01, 0.08, 0.2);
+	vec3 yellow = vec3(0.15, 0.15, 0.01);
 
 	vec3 texColor = texture( texBuf, texCoord ).rgb;
 	texColor += blue;
 
-	float x = pow((gl_FragCoord.x * sin(fTime)) / 600, 2);
-	float y = pow((gl_FragCoord.y * cos(fTime / 2)) / 400, 2);
-	texColor += yellow * sin(fTime + sin(fTime / 200 + x - y));
+	float x = cos(gl_FragCoord.x / 800  + fTime / 2);
+	float y = sin(gl_FragCoord.y / 700 + fTime);
+	texColor += yellow * pow(sin(x - y), 2);
 
-	/* modifying color based on side of the screen
+
+
+
+	// --------------- 3 waves ----------------- //
+	/*
+	vec3 blue = vec3(0.03, 0.05, 0.1);
+	vec3 yellow = vec3(0.1, 0.1, 0.01);
+
+	vec3 texColor = texture( texBuf, texCoord ).rgb;
+	texColor += blue;
+
+	float x = gl_FragCoord.x / 800 * cos(fTime / 2);
+	float y = gl_FragCoord.y / 700 * sin(fTime) + 3;
+	texColor += yellow * sin(x - y);
+	*/
+	
+	// --------------- 2 faded lights ----------------- //
+	/* 
+	vec3 blue = vec3(0.03, 0.05, 0.1);
+	vec3 yellow = vec3(0.09, 0.09, 0.01);
+
+	vec3 texColor = texture( texBuf, texCoord ).rgb;
+	texColor += blue;
+
+	float x = gl_FragCoord.x / 800 * cos(fTime / 2);
+	float y = gl_FragCoord.y / 700 * sin(fTime) + 3;
+	texColor += yellow * sin(pow(x, 2) - y);
+	*/
+	
+	// --------------- first draft of water ----------------- //
+	/* 
+	vec3 blue = vec3(0.03, 0.05, 0.1);
+	vec3 yellow = vec3(0.09, 0.09, 0.01);
+
+	vec3 texColor = texture( texBuf, texCoord ).rgb;
+	texColor += blue;
+
+	float x = (gl_FragCoord.x * cos(fTime / 2)) / 600;
+	float y = (gl_FragCoord.y * sin(fTime)) / 400;
+	texColor += yellow * sin(fTime + sin(fTime / 200 + pow(x, 2) - pow(y, 2)));
+	*/
+	
+	// --------------- modifying color based on side of the screen ----------------- //
+	/* 
 	vec3 blue = vec3(0, 0, 0.5);
 	vec3 orange = vec3(0.3, 0, 0.3);
 
 	vec3 texColor = texture( texBuf, vec2(texCoord.x + fTime, texCoord.y + 0.2f * sin(gl_FragCoord.x / 100)) ).rgb;
-	//TODO modify to show this is a 2D image
 	if (gl_FragCoord.x > 400)
 		texColor -= blue;
 	if (gl_FragCoord.x < 800)
 		texColor -= orange;
 	*/
-
-	/* orbitting light
+	
+	// --------------- orbitting light ----------------- //
+	/* 
 	vec3 yellow = vec3(0.8, 0.8, 0.0);
 	vec2 orbitPoint = vec2(640, 480);
 	float dist = distance(gl_FragCoord.xy, vec2(orbitPoint.x + 200 * sin(fTime), orbitPoint.y + 200 * cos(fTime)));
@@ -38,6 +81,8 @@ void main(){
 	vec3 texColor = texture( texBuf, texCoord ).rgb;
 	texColor += (1.0 - dist / 400) * yellow;
 	*/
+
+
 	color = vec4(texColor, 1.0);
 
 }
