efa1e87ac7decc9fb3f76f7dfe7f28eecda1534f
Night Lore <nathanlui2010@gmail.com>
2020-06-08 18:07:01 -0700

    Fix fog

--- resources/fog_fbo_frag.glsl
+++ resources/fog_fbo_frag.glsl
@@ -10,24 +10,16 @@ uniform sampler2D depthBuf;
 void main(){
 	vec3 texColor = texture( texBuf, texCoord ).rgb;
 	vec3 depth = texture( depthBuf, texCoord ).rgb;
-	vec3 fogColor = vec3(0.1, 0.15, 0.2);
+	vec3 fogColor = vec3(0.1, 0.12, 0.14);
 
 	
-	float fogDensity = 0.2;
+	float fogDensity = 5;
 	const float LOG2 = 1.442695;
-	float z = depth.r; //gl_FragCoord.z / gl_FragCoord.w;
-	float fogFactor = exp2( -fogDensity * fogDensity * z * z * LOG2 );
-	//float fogFactor = exp2( -fogDensity * z );
+	float z = 1-depth.r;
+	//float fogFactor = exp2( -fogDensity * fogDensity * z * z * LOG2 );
+	float fogFactor = exp2( -pow(fogDensity * max(z - 0.1, 0), 2) * LOG2 );
 	fogFactor = clamp(fogFactor, 0.0, 1.0);
 
 	color = vec4(mix(fogColor, texColor, fogFactor), 1.0);
-	color = vec4(vec3(texColor + fogDensity * fogColor * fogFactor), 1.0);
-	
-
-	//if (depth > 0.5)
-	//	color = vec4(mix(fogColor, vec3(texColor, 1.0), depth.r), 1.0);
-	//else
-	//	color = vec4(texColor, 1.0);
-	//color = vec4(depth, 1.0);
-	
+	//color = vec4(vec3(fogFactor), 1.0);
 }
